a75fec8b58b0b1b7d66127ef30f80897ab2d81f7
Adrian Barbosa <treceseis@MacBook-Air-de-Adrian.local>
2018-08-19 18:19:16 -0500

    'Home commit'

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,17 @@
 module.exports = {
+  'facebookAuth' : {
+    'clientID'      : '480519705750299', // your App ID
+    'clientSecret'  : 'ca29e1256db907fd039d311c16c096a2', // your App Secret
+    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+    'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+    'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+},
+googleAuth: {
+  clientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  clientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  callbackURL: "http://localhost:8080/auth/facebook/callback"
+},
   mongoURI:
     "mongodb://abarbosa:adrian100@ds137601.mlab.com:37601/automotiveadrian",
   secretOrKey: "secret"
