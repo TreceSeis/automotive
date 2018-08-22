df8933920ab8a61e47baea5de81ee787a69b1874
Adrian Barbosa <abarbosaluna@gmail.com>
2018-08-22 13:42:45 -0500

    uncomment fb passport

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-// const FacebookStrategy = require("passport-facebook").Strategy; //FB
+const FacebookStrategy = require("passport-facebook").Strategy; //FB
 
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
