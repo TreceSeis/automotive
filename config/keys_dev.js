3fb52376f14823e35efa2d9b0af0f81be428a7bf
Adrian Barbosa <abarbosaluna@gmail.com>
2018-08-23 13:47:49 -0500

    git ignore

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -3,7 +3,7 @@ module.exports = {
     "789824752239-bpf2q10ol8l22mec4hcso4njdn7stjoj.apps.googleusercontent.com",
   googleClientSecret: "iD9qI-abt9mq5Hy-yl4S6iM2",
 
-  mongoURI: "mongodb://abarbosa:adrian100@ds141631.mlab.com:37601/chachara",
+  mongoURI: "mongodb://abarbosa:adrian100@ds141631.mlab.com:41631/chachara",
 
   secretOrKey: "secret"
 };
